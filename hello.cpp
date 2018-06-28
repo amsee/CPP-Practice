f12bca1bb3289be9c80f7e1109208dad7bfd8c0d
Amy Wong <root@WINTER.localdomain>
2018-06-28 17:18:11 -0400

    adding files

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // main() is where program execution begins.
 int main() {
-	cout << "Hello World"; // prints Hello World
+	cout << "Hello World" << endl; // prints Hello World
 	return 0;
 }
+
